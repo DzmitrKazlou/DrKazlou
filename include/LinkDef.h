133038dd7b3f8f3b01bcdb677a5afb3fc25c2443
Dzmitry <103577895+DrKazlou@users.noreply.github.com>
2024-12-20 14:04:13 +0100

    Delete include/LinkDef.h

--- include/LinkDef.h
+++
@@ -1,9 +0,0 @@
-#pragma link C++ class MainFrame; 
-#pragma link C++ class LogicMenu; 
-#pragma link C++ class ParamsMenu; 
-#pragma link C++ class OptMenu; 
-#pragma link C++ class vector<vector<double> >+;
-#pragma link C++ class vector<double>+;
-//#pragma link C++ class TestMsgBox;
-//#pragma link C++ class TestSliders;
-//#pragma link C++ class TestShutter;
